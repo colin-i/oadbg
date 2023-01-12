4be91f539058c2e4d198b3eaeb17aa2e902b9038
bc <costin.botescu@gmail.com>
2023-01-12 09:23:03 +0200

    stub

---
+++ src/main.s
@@ -0,0 +1,32 @@
+
+format elfobj64
+
+const NULL=0
+
+#function closing
+#	return (FALSE)
+
+importx "gtk_application_window_new" gtk_application_window_new
+importx "gtk_window_set_title" gtk_window_set_title
+importx "gtk_widget_show" gtk_widget_show
+
+function activate(sd app)
+	sd window
+	setcall window gtk_application_window_new(app)
+	call gtk_window_set_title(window,"O Actionswf Debugger")
+	#k.g_signal_connect_data (window, b"close-request", closing, None, None, 0)
+	call gtk_widget_show(window)
+	#k.gtk_test_widget_wait_for_draw(window)
+endfunction
+
+importx "gtk_application_new" gtk_application_new
+importx "g_signal_connect_data" g_signal_connect_data
+importx "g_application_run" g_application_run
+importx "g_object_unref" g_object_unref
+
+entry main()
+	sd a;setcall a gtk_application_new((NULL),0)
+	call g_signal_connect_data(a,"activate",activate,(NULL),(NULL),0)
+	sd r;setcall r g_application_run(a,0,(NULL))
+	call g_object_unref(a)
+	return r
