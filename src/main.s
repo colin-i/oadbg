57858fe85a6eba26be3c963914c8ac9f6746f263
bc <costin.botescu@gmail.com>
2023-01-13 07:42:22 +0200

    launches

--- src/main.s
+++ src/main.s
@@ -9,12 +9,14 @@ const NULL=0
 importx "gtk_application_window_new" gtk_application_window_new
 importx "gtk_window_set_title" gtk_window_set_title
 importx "gtk_widget_show" gtk_widget_show
+importx "gtk_window_maximize" gtk_window_maximize
 
 functionx activate(sd app)
 	sd window
 	setcall window gtk_application_window_new(app)
 	call gtk_window_set_title(window,"O Actionswf Debugger")
 	#k.g_signal_connect_data (window, b"close-request", closing, None, None, 0)
+	call gtk_window_maximize(window)
 	call gtk_widget_show(window)
 	#k.gtk_test_widget_wait_for_draw(window)
 endfunction
