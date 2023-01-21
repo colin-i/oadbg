f1d5770d165a308e25008ee274e4bc587ab95174
bc <costin.botescu@gmail.com>
2023-01-21 15:20:09 +0200

    info

--- docker.sh
+++ docker.sh
@@ -3,5 +3,6 @@ cd docker && \
 docker build --tag oadbg-docker . && \
 docker cp $(docker create oadbg-docker:latest):/build/test .
 
+#docker run -t -i oadbg-docker /bin/bash
 #docker rm $(docker ps -a -f ancestor=oadbg-docker -q)
 #docker image rm oadbg-docker
