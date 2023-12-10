cd6d75b933bac4b2c19a1842e4f0d25e229ce5d0
bc <costin.botescu@gmail.com>
2023-12-10 19:31:40 +0200

    plan

--- src/oadbg.sh
+++ src/oadbg.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-#if [ -n "${1}" ]; then
-#if [ "${1}" = "d" ]; then
 #GDK_BACKEND=broadway BROADWAY_DISPLAY=:0 GTK_DEBUG=all GTK_INSPECTOR_DISPLAY=:1 ./oadbg
 #"touchscreen" is only available when building GTK with G_ENABLE_DEBUG. See GTK_DEBUG=help
 #	but there is a "simulate touchscreen" option at the inspector
-#else
-#GDK_BACKEND=broadway BROADWAY_DISPLAY=:0 ./oadbg
-#fi
-#else
-#./oadbg
-#fi
 
-#will be text:"ping" here and inside oadbg, without read inside oadbg will pe only "ping"
-#read -n10 a
-#read -n1 a
-#echo ${a} > /home/bc/Desktop/1
-#if [ "${a}" = "1" ]; then
 GDK_BACKEND=broadway BROADWAY_DISPLAY=:0 ./oadbg
-#else
-#./oadbg
-#fi
+
+#can't debug this:          browser<->(read/write stdin)oadbg
+#make a debugger extension: browser<->(read/write stdin)elf(read/write socket)<->(read/write socket)oadbg
